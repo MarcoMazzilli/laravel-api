c36bd5d68c11ce96464dc72f5200078a97eaaa66
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-06-29 20:19:32 +0200

    [Add] Fillable on model [Add] Control + save on db on `LeadController`

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
     public function store(Request $request){
         $data = $request->all();
 
-        $validator = Validator::make(
+        $validator = Validator::make( $data,
             [
                 'name' => 'required|min:2|max:255',
                 'email' => 'required|email|max:255',
@@ -29,6 +32,21 @@ class LeadController extends Controller
             ]
             );
 
-        return response()->json($data);
+            // Se i controlli falliscono
+            if($validator->fails()){
+                $success = false;
+                $errors = $validator->errors();
+                return response()->json(compact('success','errors'));
+            }
+
+
+            $new_lead = new Lead();
+            $new_lead->fill($data);
+            $new_lead->save();
+
+            Mail::to('mazzillimarco97@gmail.com')->send(new NewContact($new_lead));
+
+            $success =true;
+        return response()->json(compact('success'));
     }
 }
